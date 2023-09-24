8c4cf81aa1336ddb4e01db44e836df19908780bb
Barath-tech <barathsundar03@gmail.com>
2023-09-24 16:52:41 +0530

    Made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/globalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+const StyledApp = styled.main`
+  background-color: red;
+  padding: 2rem;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button
+          onClick={() => {
+            alert("Check In!!");
+          }}
+        >
+          Check in
+        </Button>
+        <Button
+          onClick={() => {
+            alert("Check Out!!");
+          }}
+        >
+          Check Out
+        </Button>
+        <Input placeholder="Number of Guests" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -47,3 +47,18 @@ const variations = {
     }
   `,
 };
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.4rem;
+  font-weight: 600;
+  border: none;
+  background-color: var(--color-brand-600);
+
+  color: var(--color-brand-50);
+  border-radius: var(--border-radius-sm);
+  box-shadow: var(--shadow-sm);
+  cursor: pointer;
+`;
+
+export default Button;
