b30f40b47f86fa8263a2f90896a3932cf4163162
Barath-tech <barathsundar03@gmail.com>
2023-10-06 00:24:54 +0530

    Initial  setup for dashboard is done

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,14 +1,5 @@
 import styled from "styled-components";
 
-import DurationChart from "features/dashboard/DurationChart";
-import SalesChart from "features/dashboard/SalesChart";
-import Stats from "features/dashboard/Stats";
-import TodayActivity from "features/check-in-out/TodayActivity";
-import { useRecentBookings } from "features/dashboard/useRecentBookings";
-import Spinner from "ui/Spinner";
-import { useRecentStays } from "./useRecentStays";
-import { useCabins } from "features/cabins/useCabins";
-
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
